734b6a9a88a424f807e6148951b9630c9f6f3c0a
Pathum Wijethunge <awpathum@gmail.com>
2020-04-06 17:02:55 +0530

    login component done

--- front-end-pharmacy/src/components/PharmacyApp.jsx
+++ front-end-pharmacy/src/components/PharmacyApp.jsx
@@ -1,38 +1,87 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AuthenticatedRoute from './AuthenticatedRoute.jsx';
-import HeaderComponent from "./HeaderComponent";
-import FooterComponent from "./FooterComponent";
-import LoginComponent from "./LoginComponent";
-import LogoutComponent from "./LogoutComponent";
-import WelcomeComponent from './WelcomeComponent';
+import React  from 'react';
+import { Component } from "react";
 
 class PharmacyApp extends Component{
+
     render(){
         return(
             <div className="PharmacyApp">
-                <Router>
-                    <>
-                    <HeaderComponent></HeaderComponent>
-                    <Switch>
-                    <Route path="/" exact component={LoginComponent}></Route>
-                    <Route path="/login" component={LoginComponent}></Route>
-                    <AuthenticatedRoute path="/hello" component={WelcomeComponent}></AuthenticatedRoute>
-                    {/* <AuthenticatedRoute path="/todos/:id" component={TodoComponent}></AuthenticatedRoute>
-                    <AuthenticatedRoute path="/todos" component={ListTodosComponent}></AuthenticatedRoute>
-                    <AuthenticatedRoute path="/logout" component={LogoutComponent}></AuthenticatedRoute> */}
-
-                    </Switch>
-                    <FooterComponent></FooterComponent>
-
-                    </>
-
-                </Router>
-
+            <LoginComponent></LoginComponent>
+            <WelcomeComponent></WelcomeComponent>
 
             </div>
         )
     }
 }
 
+
+class WelcomeComponent extends Component{
+
+    render(){
+        return <div>Welcome to Pharmacy</div>
+    }
+}
+
+
+class LoginComponent extends Component{
+
+    constructor(props){
+        super(props)
+        this.state ={
+            username : 'mamba',
+            password : '',
+            hasLoginFailed:false,
+            showSuccessMessage : false
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.loginClicked = this.loginClicked.bind(this);
+    }
+
+    handleChange(event){
+        //console.log(event.target.value);
+
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+
+    }
+
+    loginClicked(){
+        //mamba
+        //mamba123
+        if(this.state.username === 'mamba' && this.state.password === 'mamba123'){
+            console.log('Successful')
+            this.setState({showSuccessMessage : true})
+            this.setState({hasLoginFailed : false})
+        }
+        else{
+            console.log('failed')
+            this.setState({showSuccessMessage :false})
+            this.setState({hasLoginFailed:true})
+        }
+
+        console.log(this.state)
+    }
+     
+    render(){
+        return(
+            <div>
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {this.state.showSuccessMessage && <div>Login Successful</div>}
+                User Name : <input type="text" name="username" value={this.state.username} onChange={this.handleChange}></input>
+                Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
+                <button onClick = {this.loginClicked}>Login</button>
+
+            </div>
+
+        )
+    }
+}
+
+
+
+
+
+
 export default PharmacyApp;
+
