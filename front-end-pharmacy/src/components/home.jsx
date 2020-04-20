b5bacaa3387fc6c935c226292e3c87542510364d
Pathum Wijethunge <awpathum@gmail.com>
2020-04-21 01:04:33 +0530

    income,customer show done

--- front-end-pharmacy/src/components/home.jsx
+++ front-end-pharmacy/src/components/home.jsx
@@ -2,40 +2,62 @@ import React, { Component } from 'react';
 import ListGroup from './common/listGroup';
 import { getStocks } from '../services/stockService';
 import Warning from './warning';
+import { getBills } from '../services/billService';
 
 class Home extends Component {
     state = {
         stocks: [],
         expStocks: [],
+        bills: [],
+        todayIncome: 0,
+        customerCount: 0
 
 
     }
 
     async componentDidMount() {
-        const { data } = await getStocks();
+        const { data: stocks } = await getStocks();
+        const { data: bills } = await getBills();
 
         this.setState({
-            stocks: data
+            stocks,
+            bills
         })
         this.getWarnings();
-        this.getCurrentDate();
+        this.getTodayIncome();
     }
 
+
+
     getWarnings = () => {
-        const today = this.getCurrentDate();
+        const today = this.getNotificationDate();
         console.log(today)
-        const filtered = this.state.stocks.filter( s => s.quantity != 0);
-        console.log(filtered)
+        const filtered = this.state.stocks.filter(s => s.quantity != 0);
+        console.log('filterd', filtered)
         let expStocks = [];
-        filtered.map(d => ((Date.parse(d.expDate) < Date.parse(today)) ? expStocks.push(<Warning expDate = {d.expDate}></Warning>) : console.log('not expired', d.expDate)))
+        filtered.map(d => ((Date.parse(d.expDate) < Date.parse(today)) ? expStocks.push(<Warning expDate={d.expDate} drugName={d.drugName} Drug={d}></Warning>) : console.log('not expired', d.expDate)))
         this.setState({
             expStocks
         })
     }
 
+    getNotificationDate = () => {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        // today.getMonth() + 1) this gives today date 
+        var mm = String(today.getMonth() + 2).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        // today = mm + '-' + dd + '-' + yyyy;
+        today = yyyy + '-' + mm + '-' + dd;
+        // console.log('today', today);
+        return today;
+    }
+
     getCurrentDate = () => {
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
+        // today.getMonth() + 1) this gives today date 
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = today.getFullYear();
 
@@ -45,14 +67,41 @@ class Home extends Component {
         return today;
     }
 
+    getTodayIncome = () => {
+        const today = this.getCurrentDate();
+        const filtered = this.state.bills.filter(b => (b.date === today));
+        const customerCount = filtered.length;
+        let income = [];
+        filtered.map(b => income.push(b.totalPrice));
+        console.log('filtered', filtered)
+        let todayIncome = income.reduce((partial_sum, a) => partial_sum + a, 0);
+        console.log("todayIncome", todayIncome)
+        this.setState({ todayIncome, customerCount })
+
+    }
+
+    //     <div class="row">
+    //   <div class="col-12 col-md-8">.col-12 .col-md-8</div>
+    //   <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+    // </div>
+
     render() {
         console.log(this.state.expStocks)
         return (
+
             <div className="row">
-                <h1>Home</h1>
-                <div className="col-2">
-                    {this.state.expStocks}
+                <div className="col-6 col-md-4">
+                    <ul className="list-group">
+                        {this.state.expStocks}
+                    </ul>
                 </div>
+                <div className="col-12 col-md-8">
+                    <span class="badge badge-info"><h3>Today's income {this.state.todayIncome} rupees.</h3></span>
+                    <br></br>
+                    <span class="badge badge-success"><h3>Number of Customers {this.state.customerCount} </h3></span>
+
+                </div>
+
 
             </div>
         );
--- front-end-pharmacy/src/components/warning.jsx
+++ front-end-pharmacy/src/components/warning.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Warning extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    state = {
+        today: '',
+    }
+
+    componentDidMount() {
+        this.getCurrentDate();
+    }
+    getCurrentDate = () => {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        // today.getMonth() + 1) this gives today date 
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        // today = mm + '-' + dd + '-' + yyyy;
+        today = yyyy + '-' + mm + '-' + dd;
+        // console.log('today', today);
+        this.setState({
+            today
+        })
+    }
+
+    render() {
+
+
+        return (
             <div class="card">
-            <div class="card-body">
-        <h5 class="card-title">{this.props.expDate}</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="card-body">
+                    {(this.state.today > this.props.Drug.expDate) ? <h5 class="card-title text-danger">{this.props.Drug.drugName}</h5> : <h5 class="card-title text-warning">{this.props.Drug.drugName}</h5>}
+
+        <p class="card-text"> {this.props.Drug.drugName} stock is going to expire on {this.props.Drug.expDate}. Quantity is {this.props.Drug.quantity}</p>
+                    {/* <a href="#" class="btn btn-primary">See Stock</a> */}
+                    <Link to={`/stocks/${this.props.Drug.id}`}>Go To Stock</Link>
+                </div>
             </div>
-          </div>
-         );
+        );
     }
 }
- 
+
 export default Warning;
