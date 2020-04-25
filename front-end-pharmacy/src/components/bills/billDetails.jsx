f3cf115e985c799dcfe36d67c48e62cf3b1a5156
Pathum Wijethunge <awpathum@gmail.com>
2020-04-25 19:24:45 +0530

    display drugs in bill

--- front-end-pharmacy/src/components/bills/billDetails.jsx
+++ front-end-pharmacy/src/components/bills/billDetails.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Formik, Form, Field, } from 'formik';
-import {getBill,getDrugs} from '../../services/billService';
+import { getBill } from '../../services/billService';
+import { getDrugs } from '../../services/drugService';
 class BillDetails extends Component {
     state = {
         data: {
@@ -8,16 +9,17 @@ class BillDetails extends Component {
             date: "",
             customerName: "",
             customerAge: "",
-            totalPrice:0,
-            drugBills:[]
-        }
+            totalPrice: 0,
+            drugBills: []
+        },
+        drugs: []
     }
 
     async populateBill() {
         const billId = this.props.match.params.id;
         try {
             const { data: bill } = await getBill(billId);
-            console.log('data',bill)
+            console.log('data', bill)
 
             this.setState({
                 data: this.mapToViewModel(bill)
@@ -29,15 +31,18 @@ class BillDetails extends Component {
 
     }
 
-    async populateDrugs(){
-        const billId = this.props.match.params.id;
+    async populateDrugs() {
+
         try {
-            const{data:drugs} = await getDrugs(billId);
-            console.log(drugs)
+            const { data: drugs } = await getDrugs();
+            console.log('drugs', drugs);
+            this.setState({ drugs });
         } catch (ex) {
-            
+            if (ex.response && ex.response.status === 404)
+                this.props.history.replace("/not-found");
         }
     }
+
 
     async componentDidMount() {
         await this.populateBill();
@@ -50,58 +55,62 @@ class BillDetails extends Component {
             date: bill.date,
             customerName: bill.customerName,
             customerAge: bill.customerAge,
-            totalPrice:bill.totalPrice,
-            drug : bill.drugBills[0].id.drugId
+            totalPrice: bill.totalPrice,
+            drugBills: bill.drugBills
         };
     }
 
     render() {
-        const { data } = this.state;
+        const { data,drugs } = this.state;
         console.log(data)
-        console.log(data.drugBills)
+        console.log(drugs)
+        console.log('drugBills', data.drugBills)
         return (
             <div className='container'>
-            <Formik
-            enableReinitialize
-                initialValues={{
-                    id: data.id,
-                    date: data.date,
-                    customerName:data.customerName,
-                    customerAge:data.customerAge,
-                    totalPrice : data.totalPrice
-                }}   
-            >
-                {
-                    (props) => (
-                        <Form>
-                            <fieldset className="form-group">
-                                <label>Bill Id</label>
-                                <Field className="form-control" type="text" name="id" disabled></Field>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <label>Date</label>
-                                <Field className="form-control" type="date" name="date" disabled></Field>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <label>Total Price</label>
-                                <Field className="form-control" type="text" name="totalPrice" disabled></Field>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <label>Customer Name</label>
-                                <Field className="form-control" type="text" name="customerName" disabled></Field>
-                            </fieldset>
-                            <fieldset className="form-group">
-                                <label>Customer Age</label>
-                                <Field className="form-control" type="text" name="customerAge" disabled></Field>
-                            </fieldset>
-                            {this.state.drugBills}
-                        </Form>
-                    )
-                }
-            </Formik>
+                <Formik
+                    enableReinitialize
+                    initialValues={{
+                        id: data.id,
+                        date: data.date,
+                        customerName: data.customerName,
+                        customerAge: data.customerAge,
+                        totalPrice: data.totalPrice
+                    }}
+                >
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Bill Id</label>
+                                    <Field className="form-control" type="text" name="id" disabled></Field>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Date</label>
+                                    <Field className="form-control" type="date" name="date" disabled></Field>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Total Price</label>
+                                    <Field className="form-control" type="text" name="totalPrice" disabled></Field>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Customer Name</label>
+                                    <Field className="form-control" type="text" name="customerName" disabled></Field>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Customer Age</label>
+                                    <Field className="form-control" type="text" name="customerAge" disabled></Field>
+                                </fieldset>
+                        {data.drugBills.map(d => <div class="overflow-auto">{d.id.drugId}&nbsp;&nbsp;&nbsp;{d.quantity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{d.netPrice}</div>)}
+
+                            </Form>
+                        )
+                    }
+                </Formik>
             </div>
         );
     }
 }
 
 export default BillDetails;
+
+// { drugs.filter(d => d.id === d.id.drugId) }
